0d026b3301e6742eb5a3cc7724477d9ef2ad27a2
User <tim.sim4@yandex.ru>
2023-06-30 15:56:38 +0700

    Прикрутил авторизацию в firebase

--- app/src/main/java/com/example/mypsychologist/MainActivity.kt
+++ app/src/main/java/com/example/mypsychologist/MainActivity.kt
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import com.example.mypsychologist.databinding.ActivityMainBinding
 import com.example.mypsychologist.ui.main.MainFragment
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity(), NavbarHider {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        onSignInResult(res)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +31,38 @@ class MainActivity : AppCompatActivity(), NavbarHider {
 
         getAppComponent().inject(this)
 
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null)
+            createSignInIntent()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.Theme_MyPsychologist)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val message = if (result.resultCode == RESULT_OK) {
+            R.string.hallow
+        } else {
+            R.string.network_error
+        }
+        Toast.makeText(this, getString(message), Toast.LENGTH_LONG).show()
+    }
+
 
     override fun setNavbarVisibility(it: Boolean) {
         binding.navigation.isVisible = it
--- app/src/main/java/com/example/mypsychologist/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mypsychologist/ui/main/MainFragment.kt
@@ -1,15 +1,16 @@
 package com.example.mypsychologist.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mypsychologist.R
 import com.example.mypsychologist.databinding.FragmentMainBinding
 import com.example.mypsychologist.presentation.MainViewModel
 import com.example.mypsychologist.setupCard
+
 
 class MainFragment : Fragment() {
 
