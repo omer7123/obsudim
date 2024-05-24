0fcb523050fd3dc397809addf8854a54288f6a80
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2024-05-24 21:33:03 +0700

    Добавил Body

--- app/src/main/java/com/example/mypsychologist/data/remote/AuthenticationService.kt
+++ app/src/main/java/com/example/mypsychologist/data/remote/AuthenticationService.kt
@@ -3,9 +3,10 @@ package com.example.mypsychologist.data.remote
 import com.example.mypsychologist.data.model.RegisterModel
 import com.example.mypsychologist.data.model.UserModel
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthenticationService {
     @POST("/api/users/register")
-    suspend fun register(register: RegisterModel): Response<UserModel>
+    suspend fun register(@Body register: RegisterModel): Response<UserModel>
 }
