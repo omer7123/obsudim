4391a56f81f4407c9646e3b37e5b347c5912e2c9
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2025-01-30 00:38:47 +0300

    исправил баг на экране с практикой

--- app/src/main/java/com/example/mypsychologist/ui/exercises/FragmentExercises.kt
+++ app/src/main/java/com/example/mypsychologist/ui/exercises/FragmentExercises.kt
@@ -16,7 +16,6 @@ import com.example.mypsychologist.R
 import com.example.mypsychologist.databinding.FragmentExercisesBinding
 import com.example.mypsychologist.domain.entity.exerciseEntity.ExerciseEntity
 import com.example.mypsychologist.extensions.getAppComponent
-import com.example.mypsychologist.extensions.isNetworkConnect
 import com.example.mypsychologist.extensions.showToast
 import com.example.mypsychologist.presentation.core.BaseStateUI
 import com.example.mypsychologist.presentation.exercises.exercisesFragment.REBTViewModel
@@ -25,8 +24,7 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
-class
-FragmentExercises : Fragment() {
+class FragmentExercises : Fragment() {
 
     private var binding: FragmentExercisesBinding by autoCleared()
 
@@ -42,18 +40,13 @@ FragmentExercises : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentExercisesBinding.inflate(inflater, container, false)
 
-        viewModel.screenState
-            .flowWithLifecycle(lifecycle)
-            .onEach { state ->
+        viewModel.screenState.flowWithLifecycle(lifecycle).onEach { state ->
                 render(state)
-            }
-            .launchIn(lifecycleScope)
+            }.launchIn(lifecycleScope)
 
         initListener()
 
@@ -64,19 +57,19 @@ FragmentExercises : Fragment() {
         when (state) {
             is BaseStateUI.Content -> {
                 exercisesList.addAll(state.data)
+                binding.content.isVisible = true
                 binding.networkPlaceholder.layout.isVisible = false
                 binding.progressCircular.isVisible = false
             }
 
             is BaseStateUI.Loading -> {
-                if (!isNetworkConnect()) {
-                    binding.networkPlaceholder.layout.isVisible = true
-                } else {
-                    binding.progressCircular.isVisible = true
-                }
+                binding.content.isVisible = false
+                binding.progressCircular.isVisible = true
+
             }
 
             is BaseStateUI.Error -> {
+                binding.content.isVisible = true
                 binding.networkPlaceholder.layout.isVisible = false
                 binding.progressCircular.isVisible = false
 
