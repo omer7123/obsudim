ed48c218edfc0bd6b23482f2044989448c505456
desman4 <timofejsimonov@gmail.com>
2025-02-03 12:28:30 +0700

    Поправил цвета темной темы

--- app/src/main/java/com/example/mypsychologist/ui/authentication/authFragment/AuthFragment.kt
+++ app/src/main/java/com/example/mypsychologist/ui/authentication/authFragment/AuthFragment.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
