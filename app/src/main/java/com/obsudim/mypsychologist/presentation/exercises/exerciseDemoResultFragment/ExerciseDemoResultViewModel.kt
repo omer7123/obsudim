f4e8b836b218b9617f1678c555d3590fa0705dda
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2025-11-24 08:27:44 +0700

    FE-871 Создал стейт для экрана просмотра результата упражнения и обновляю его при получении данных

--- app/src/main/java/com/obsudim/mypsychologist/presentation/exercises/exerciseDemoResultFragment/ExerciseDemoResultViewModel.kt
+++ app/src/main/java/com/obsudim/mypsychologist/presentation/exercises/exerciseDemoResultFragment/ExerciseDemoResultViewModel.kt
@@ -5,18 +5,31 @@ import androidx.lifecycle.viewModelScope
 import com.obsudim.mypsychologist.core.Resource
 import com.obsudim.mypsychologist.domain.entity.exerciseEntity.ExerciseDetailResultEntity
 import com.obsudim.mypsychologist.domain.useCase.exerciseUseCases.GetExerciseDetailResultUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ExerciseDemoResultViewModel @Inject constructor(private val getExerciseDetailResultUseCase: GetExerciseDetailResultUseCase) :
     ViewModel() {
+    private val _screenState: MutableStateFlow<ExerciseDemoResultScreenState> = MutableStateFlow(ExerciseDemoResultScreenState.Initial)
+    val screenState = _screenState.asStateFlow()
+
     fun getExerciseResult(idExercise: String, idResult: String) {
         viewModelScope.launch {
             getExerciseDetailResultUseCase(idExercise, idResult).collect { state ->
                 when (state) {
-                    is Resource.Error<ExerciseDetailResultEntity> -> {}
-                    Resource.Loading -> {}
-                    is Resource.Success<ExerciseDetailResultEntity> -> {}
+                    is Resource.Error<ExerciseDetailResultEntity> -> {
+                        _screenState.value = ExerciseDemoResultScreenState.Error
+                    }
+                    Resource.Loading -> {
+                        _screenState.value = ExerciseDemoResultScreenState.Loading
+                    }
+                    is Resource.Success<ExerciseDetailResultEntity> -> {
+                        _screenState.value = ExerciseDemoResultScreenState.Content(
+                            state.data.sections
+                        )
+                    }
                 }
             }
         }
