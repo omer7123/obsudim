5c87c736bafa1b797639fa101d7720b72b020f44
llikem <vlad190718@gmail.com>
2025-12-22 01:57:10 +0700

    FE-905 Актуализировал данные UserInfoModel.kt

--- app/src/main/java/com/obsudim/mypsychologist/domain/entity/priofileEntity/UserInfoEntity.kt
+++ app/src/main/java/com/obsudim/mypsychologist/domain/entity/priofileEntity/UserInfoEntity.kt
@@ -1,27 +1,17 @@
 package com.obsudim.mypsychologist.domain.entity.priofileEntity
 
-data class UserInfoEntity(
-    val birthDate: String,
-    val gender: String,
-    val username: String,
-    val request: List<Int>,
-    val city: String,
-    val description: String,
-    val type: Int
-)
-
 data class UserDataEntity(
     val id: String,
     val username: String,
     val email: String,
     val city: String,
     val company: String?,
-    val online: String?,
+    val online: Boolean,
     val gender: String,
     val birthDate: String,
     val phoneNumber: String,
     val description: String?,
-    val isActive: String?,
+    val isActive: Boolean,
     val department: String?,
     val faceToFace: Boolean?,
     val roleId: Int,
