cf4be2c08ab460b8af9399de2a117bcb0773d183
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2026-02-20 20:26:39 +0300

    PRO-960 Добавил интент на открытие приложения по нажатию на уведомление

--- app/src/main/java/com/obsudim/mypsychologist/data/repository/notification/NotificationSenderImpl.kt
+++ app/src/main/java/com/obsudim/mypsychologist/data/repository/notification/NotificationSenderImpl.kt
@@ -2,9 +2,12 @@ package com.obsudim.mypsychologist.data.repository.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.obsudim.mypsychologist.MainActivity
 import com.obsudim.mypsychologist.R
 import com.obsudim.mypsychologist.domain.repository.NotificationSender
 import javax.inject.Inject
@@ -12,6 +15,17 @@ import javax.inject.Inject
 class NotificationSenderImpl @Inject constructor(
     private val context: Context
 ): NotificationSender {
+
+    private val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    private val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
 
     override fun send(title: String, text: String) {
         val channelId = "daily_channel"
@@ -33,6 +47,7 @@ class NotificationSenderImpl @Inject constructor(
             .setContentTitle(title)
             .setContentText(text)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
             .build()
 
         manager.notify(1001, notification)
