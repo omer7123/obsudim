2895f689513ab33cce2f2462450ab20bcc56ffdf
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2026-02-20 17:10:11 +0300

    PRO-960 Настроил инициализаацию WorkManager через фабрику и добавил создание зависимостей

--- app/src/main/java/com/obsudim/mypsychologist/App.kt
+++ app/src/main/java/com/obsudim/mypsychologist/App.kt
@@ -1,19 +1,34 @@
 package com.obsudim.mypsychologist
 
 import android.app.Application
+import androidx.work.Configuration
 import com.google.firebase.FirebaseApp
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.obsudim.mypsychologist.di.AppComponent
 import com.obsudim.mypsychologist.di.DaggerAppComponent
+import com.obsudim.mypsychologist.di.DailyNotificationWorkerFactory
+import javax.inject.Inject
 
-class App: Application() {
+class App : Application(), Configuration.Provider {
 
     lateinit var appComponent: AppComponent
+
+    @Inject
+    lateinit var workerFactory: DailyNotificationWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
         AndroidThreeTen.init(this)
         appComponent = DaggerAppComponent.factory().create(applicationContext)
+
+        appComponent.inject(this)
+
+
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
